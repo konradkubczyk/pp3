ffd2e63d932e01ddb262d31b94e1901d30d407e2
Konrad Kubczyk <konrad@kubczyk.com>
2022-10-17 22:35:52 +0200

    Add solution for exercise 1.2 from the coursebook

---
+++ 01-CourseIntroduction/HelloMultiline.java
@@ -0,0 +1,6 @@
+public class HelloMultiline {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!"); // Display first line
+		System.out.println("How are you?"); // Display second line
+	}
+}
