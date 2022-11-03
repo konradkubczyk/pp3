b35efdc3ba9dbafb41bedce5ceada70af418a8d8
Konrad Kubczyk <konrad@kubczyk.com>
2022-11-03 23:16:23 +0100

    Update method naming convention

--- 03-ClassStructure/14/TemperatureUnitConverter.java
+++ 03-ClassStructure/14/TemperatureUnitConverter.java
@@ -1,26 +1,26 @@
 class TemperatureUnitConverter {
 
-    public double CelsiusToKelvin(double celsius) {
+    public double celsiusToKelvin(double celsius) {
         return celsius + 273.15;
     }
 
-    public double CelsiusToFahrenheit(double celsius) {
+    public double celsiusToFahrenheit(double celsius) {
         return celsius * 9 / 5 + 32;
     }
 
-    public double KelivnToCelsius(double kelvin) {
+    public double kelivnToCelsius(double kelvin) {
         return kelvin - 273.15;
     }
 
-    public double KelvinToFahrenheit(double kelvin) {
+    public double kelvinToFahrenheit(double kelvin) {
         return (kelvin - 273.15) * 9 / 5 + 32;
     }
 
-    public double FahrenheitToCelsius(double fahrenheit) {
+    public double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
 
-    public double FahrenheitToKelvin(double fahrenheit) {
+    public double fahrenheitToKelvin(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9 + 273.15;
     }
 
@@ -28,12 +28,12 @@ class TemperatureUnitConverter {
 
         TemperatureUnitConverter temperatureUnitConverter = new TemperatureUnitConverter();
 
-        System.out.println("25 °C = " + temperatureUnitConverter.CelsiusToKelvin(25) + " K");
-        System.out.println("25 °C = " + temperatureUnitConverter.CelsiusToFahrenheit(25) + " °F");
-        System.out.println("100 °F = " + temperatureUnitConverter.FahrenheitToKelvin(100) + " K");
-        System.out.println("100 °F = " + temperatureUnitConverter.FahrenheitToCelsius(100) + " °C");
-        System.out.println("0 K = " + temperatureUnitConverter.KelivnToCelsius(0) + " °C");
-        System.out.println("0 K = " + temperatureUnitConverter.KelvinToFahrenheit(0) + " °F");
+        System.out.println("25 °C = " + temperatureUnitConverter.celsiusToKelvin(25) + " K");
+        System.out.println("25 °C = " + temperatureUnitConverter.celsiusToFahrenheit(25) + " °F");
+        System.out.println("100 °F = " + temperatureUnitConverter.fahrenheitToKelvin(100) + " K");
+        System.out.println("100 °F = " + temperatureUnitConverter.fahrenheitToCelsius(100) + " °C");
+        System.out.println("0 K = " + temperatureUnitConverter.kelivnToCelsius(0) + " °C");
+        System.out.println("0 K = " + temperatureUnitConverter.kelvinToFahrenheit(0) + " °F");
 
     }
 
