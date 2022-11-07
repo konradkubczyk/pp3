d313295a4ddcad3d2b8009d483b55ec4268232c5
Konrad Kubczyk <konrad@kubczyk.com>
2022-11-08 00:28:51 +0100

    Create toString() method and update directory name

--- 03-ClassStructure/22 23/Room.java
+++ 03-ClassStructure/22 23/Room.java
@@ -43,32 +43,28 @@ public class Room {
         );
     }
 
-    public static void displayStatusInOneRow(Room room) {
-        System.out.println("Room " + room.number + " has " + room.beds + " bed(s) and is currenty " + (room.occupied ? "occupied" +
-        " by " + room.guestName + "." : "vacant."));
-    }
-
     public static void displayRoomsReport(Room[] rooms) {
+        
         System.out.println("Rooms' state report:");
         
         for (int i = 0; i < rooms.length; i++) {
-            System.out.println("");
-            rooms[i].displayStatus();
+            System.out.println(rooms[i]);
         }
     }
 
     public static void displayRoomsWithNBeds(Room[] rooms, int numberOfBeds) {
+        
         System.out.println("Rooms with " + numberOfBeds + " beds:");
         
         for (int i = 0; i < rooms.length; i++) {
             if (rooms[i].beds == numberOfBeds) {
-                System.out.println("");
-                rooms[i].displayStatus();
+                System.out.println(rooms[i]);
             }
         }
     }
 
     public static void displayCountOfOccupiedAndVacantRooms(Room[] rooms) {
+        
         int occupiedRoomsCount = 0;
 
         for (int i = 0; i < rooms.length; i++) {
@@ -92,6 +88,11 @@ public class Room {
         System.out.println("There are " + vacantBedsCount + " vacant beds.");
     }
 
+    public String toString() {
+        return "Room " + this.number + " has " + this.beds + " bed(s) and is currenty " + (this.occupied ? "occupied" +
+        " by " + this.guestName + "." : "vacant.");
+    }
+
     public static void main(String[] args) {
         Room[] rooms = new Room[6];
 
@@ -105,17 +106,15 @@ public class Room {
         rooms[0].checkin("Alice");
         rooms[2].checkin("Bob");
 
-        System.out.println();
-
         // rooms[1].checkout(); // throws IllegalStateException (room already empty)
         // rooms[0].checkin("Jeff"); // throws IllegalStateException (room already occupied)
 
-        Room.displayStatusInOneRow(rooms[0]);
-        System.out.println("\n");
+        System.out.println();
+        System.out.println(rooms[0] + "\n");
         Room.displayRoomsReport(rooms);
-        System.out.println("\n");
+        System.out.println();
         Room.displayRoomsWithNBeds(rooms, 2);
-        System.out.println("\n");
+        System.out.println();
         Room.displayCountOfOccupiedAndVacantRooms(rooms);
         System.out.println();
         Room.displayCountOfVacantBeds(rooms);
