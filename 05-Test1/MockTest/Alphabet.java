29eef11b0a533a1437db9f50f7014919f3ba05c9
Konrad Kubczyk <konrad@kubczyk.com>
2022-11-15 13:42:37 +0100

    Update Mock Test 1 solution

--- 05-Test1/MockTest/Alphabet.java
+++ 05-Test1/MockTest/Alphabet.java
@@ -1,7 +1,7 @@
 public class Alphabet {
     public static boolean isAlphabet(String t) {
         for (int i = 1; i < t.length(); i++) {
-            if (t.charAt(i) - t.charAt(i-1) < 0) {
+            if (Character.toLowerCase(t.charAt(i)) - Character.toLowerCase(t.charAt(i-1)) < 0) {
                 return false;
             }
         }
@@ -9,7 +9,7 @@ public class Alphabet {
     }
 
     public static void main(String[] args) {
-        System.out.println(Alphabet.isAlphabet("abegsw"));
+        System.out.println(Alphabet.isAlphabet("aAbegsw"));
         System.out.println(Alphabet.isAlphabet("abcmhsw"));
     }
 }
