70dc9bf67734614bcb998e4b30ce9ca645c20f3a
Konrad Kubczyk <konrad@kubczyk.com>
2022-11-28 19:21:19 +0100

    Add solution for ex. 13

---
+++ 06-ConceptualModeling/13/Book.java
@@ -0,0 +1,106 @@
+public class Book {
+    public static String[] genres = { "crime", "drama", "fantasy", "science fiction" };
+    private String title;
+    private String author;
+    private String genre;
+    private int pages;
+    private int publicationYear;
+    private String publisher;
+    private String isbn;
+    private String description;
+
+    public static String[] getGenres() {
+        return genres;
+    }
+
+    public static void setGenres(String[] genres) {
+        Book.genres = genres;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Book(String title, String author, String genre, int pages, int publicationYear, String publisher, String isbn, String description) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+        this.publicationYear = publicationYear;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.description = description;
+    }
+
+    public String toString() {
+        return "Book details:" +
+                "\nTitle: " + title +
+                "\nAuthor: " + author +
+                "\nGenre: " + genre +
+                "\nPages: " + pages +
+                "\nPublication year: " + publicationYear +
+                "\nPublisher: " + publisher +
+                "\nISBN: " + isbn +
+                "\nDescription: " + description;
+    }
+}
