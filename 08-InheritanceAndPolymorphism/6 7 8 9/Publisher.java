0bec40f2138cfb5d5d5194f6adb1717969491f4f
Konrad Kubczyk <konrad@kubczyk.com>
2022-12-12 22:06:54 +0100

    Add solution for ex. 10

---
+++ 08-InheritanceAndPolymorphism/6 7 8 9/Publisher.java
@@ -0,0 +1,26 @@
+public class Publisher {
+    private String name;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Publisher(String name, String city) {
+        this.setName(name);
+        this.setCity(city);
+    }
+
+}
