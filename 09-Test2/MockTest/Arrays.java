246082b2111219eb0479011000e6ff1f67feb088
Konrad Kubczyk <konrad@kubczyk.com>
2022-12-13 00:19:15 +0100

    Refactor to improve code quality

--- 09-Test2/MockTest/Arrays.java
+++ 09-Test2/MockTest/Arrays.java
@@ -1,21 +1,17 @@
 public class Arrays {
+    private static int countDoubleDigits(int[] array) {
+        int doubleDigitCount = 0;
+
+        for (int integer : array) {
+            if (Integer.toString(integer).length() == 2) {
+                doubleDigitCount++;
+            }
+        }
+
+        return doubleDigitCount;
+    }
+
     public static Boolean arr(int[] arr1, int[] arr2) {
-        int arr1Count = 0;
-
-        for (int i = 0; i < arr1.length; i++) {
-            if (Integer.toString(arr1[i]).length() == 2) {
-                arr1Count++;
-            }
-        }
-
-        int arr2Count = 0;
-
-        for (int i = 0; i < arr2.length; i++) {
-            if (Integer.toString(arr2[i]).length() == 2) {
-                arr2Count++;
-            }
-        }
-
-        return arr1Count == arr2Count;
+        return countDoubleDigits(arr1) == countDoubleDigits(arr2);
     }
 }
